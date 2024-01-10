762eb04a01a3e3d4644d60ba2e91aed929011a49
hzyu17 <hz17.yu@gmail.com>
2024-01-10 12:43:31 -0500

    namespace gvi

--- gp/factorized_opts_linear.h
+++ gp/factorized_opts_linear.h
@@ -2,6 +2,8 @@
 
 #include "ngd/NGDFactorizedLinear.h"
 #include "ngd/NGDFactorizedFixedGaussian.h"
+#include "gp/fixed_prior.h"
+#include "gp/minimum_acc_prior.h"
 
 namespace gvi{
     using FixedGpPrior = NGDFactorizedFixedGaussian<FixedPriorGP>;
