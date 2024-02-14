307ed74cae84490d8c0c4b59f04b0549194d90e8
hzyu17 <hz17.yu@gmail.com>
2024-02-14 13:16:32 -0500

    debugging the serialization of iteration data

--- helpers/test_serialization.h
+++ helpers/test_serialization.h
@@ -18,6 +18,10 @@ public:
     void set_nt(int nt){ _nt = nt; }
     void set_mat(const Matrix3D& mat){ _mat = mat; }
 
+    int nt() const { return _nt; }
+    int dim() const { return _dim; }
+    Matrix3D mat() const {return _mat; }
+
     void save_data(const std::string& file_name){
         // Save the tuple to a binary file
         std::ofstream ofs(file_name, std::ios::binary);
@@ -60,72 +64,94 @@ protected:
 };
 
 
+template<typename DataType>
+class IterationData{
+    typedef std::unordered_map<int, DataType> IterDataMap;
+public:
+    IterationData(int niters, int dim, int nt): _niters{niters}, _dim{dim}{}
 
-// template<typename DataType>
-// class IterationData{
-// using IterDataMap = std::unordered_map<int, DataType>;
+    IterationData(){}
 
-// public:
-//     IterationData(int niters, int dim, int nt): _niters{niters}, _dim{dim}{}
+    void add_data(int iter, const DataType& data){
+        _map[iter] = data;
+    }
 
-//     IterationData(){}
+    int size() const {
+        return _map.size();
+    }
 
-//     void add_data(int iter, const DataType& data){
-//         _map[iter] = data;
-//     }
+    int dim() const {
+        return _dim;
+    }
 
-//     int size(){
-//         int map_size = _map.size();
-//         return map_size;
-//     }
+    int niters() const {
+        return _niters;
+    }
 
-//     void save_data(const std::string& file_name){
-//         // Save the tuple to a binary file
-//         std::ofstream ofs(file_name, std::ios::binary);
-//         boost::archive::binary_oarchive oa(ofs);
-//         oa << _map;
-//     }
-
-//     void print_data(){
-//         for(auto & pair:_map){
-//             int key = pair.first;
-//             DataType data = pair.second;
-//             std::cout << "Key: " << key << ", " << std::endl << "Value: " << std::endl;
-//             data.print_data();
-//         }
-//     }
-
-//     void load_data(const std::string& file_name){
-//         try {
-//             std::ifstream ifs(file_name, std::ios::binary);
-//             if (!ifs.is_open()) {
-//                 throw std::runtime_error("Failed to open file of iteration data. \n File: " + file_name);
-//             }
-
-//             boost::archive::binary_iarchive ia(ifs);
-//             ia >> _map;
-
-//         } catch (const boost::archive::archive_exception& e) {
-//             std::cerr << "Boost archive exception: " << e.what() << std::endl;
-//         } catch (const std::exception& e) {
-//             std::cerr << "Standard exception: " << e.what() << std::endl;
-//         }
-//     }
+    IterDataMap map() const {
+        return _map;
+    }
 
 
-// protected:
-//     IterDataMap _map;
-//     int _niters, _dim;
+    void save_data(const std::string& file_name) const {
+        // Save the tuple to a binary file
+        std::ofstream ofs(file_name, std::ios::binary);
+        boost::archive::binary_oarchive oa(ofs);
+        oa << _map;
+        ofs.close();
+    }
 
-// };
+    void print_data() const{
+        for(auto & pair:_map){
+            int key = pair.first;
+            DataType data = pair.second;
+            std::cout << "Key: " << key << ", " << std::endl << "Value: " << std::endl;
+            data.print_data();
+        }
+    }
 
-// typedef IterationData<MyData> IterationMydata;
+    void load_data(const std::string& file_name){
+        try {
+            std::ifstream ifs(file_name, std::ios::binary);
+            if (!ifs.is_open()) {
+                throw std::runtime_error("Failed to open file of iteration data. \n File: " + file_name);
+            }
+
+            boost::archive::binary_iarchive ia(ifs);
+            ia >> _map;
+
+        } catch (const boost::archive::archive_exception& e) {
+            std::cerr << "Boost archive exception: " << e.what() << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Standard exception: " << e.what() << std::endl;
+        }
+    }
+
+    void set_map(const IterDataMap& map){
+        _map = map;
+    }
+
+    void set_niters(int niters){
+        _niters = niters;
+    }
+
+    void set_dim(int dim){
+        _dim = dim;
+    }
+
+protected:
+    IterDataMap _map;
+    int _niters, _dim;
+
+};
+
+typedef IterationData<MyData> IterationMydata;
 
 }
 
 using namespace gvi;
 
-// Define the serialization of iteration data type.
+// Define the serialization of MyData type.
 namespace boost {
    namespace serialization {        
         template<class Archive>
@@ -133,9 +159,9 @@ namespace boost {
                   const MyData& data, 
                   const unsigned int version) 
         {   
-            int nt{data._nt};
-            int dim{data._dim};
-            MatrixXd mat{data._mat};
+            int nt = data.nt();
+            int dim = data.dim();
+            MatrixXd mat{data.mat()};
 
             ar & nt;
             ar & dim;
@@ -173,58 +199,111 @@ namespace boost {
     }
 }
 
-
-// // Define the serialization of iteration data type.
-// namespace boost {
-//    namespace serialization {  
-
-//         template<class Archive>
-//         void save(Archive& ar, 
-//                   const IterationMydata& map, 
-//                   const unsigned int version) 
-//         {
-//             int size = map._map.size();
+// Define the serialization of IterDataMap<DataType> type.
+namespace boost {
+   namespace serialization {        
+        template<class Archive, class DataType>
+        void save(Archive& ar, 
+                  const std::unordered_map<int, DataType>& map, 
+                  const unsigned int version) 
+        {   
+            int size = 0;
+            size = map.size();
             
-//             ar & size;
+            ar & size;
 
-//             for (auto & pair: map._map) { 
-//                 int key = pair.first;
-//                 MyData value = pair.second;
+            for (auto & pair: map) { 
+                int key = pair.first;
+                DataType value = pair.second;
 
-//                 ar & key; // Serialize first int key
-//                 ar & value; // Serialize MyData value
+                ar & key; // Serialize first int key
+                ar & value; // Serialize MyData value
 
-//             }
-//         }
+            }
+        }
 
-//         template<class Archive>
-//         void load(Archive& ar, IterationMydata& map, const unsigned int version) {
-//             int size = 0;
-//             ar & size;
+        template<class Archive, class DataType>
+        void load(Archive& ar, std::unordered_map<int, DataType>& map, const unsigned int version) {
+            int size = 0;
+            ar & size;
 
-//             for (int i=0; i< size; i++) { 
+            for (int i=0; i< size; i++) { 
 
-//                 int key = 0;
-//                 MyData data;
+                int key = 0;
+                DataType data;
 
-//                 ar & key; // Serialize first double in the key tuple
-//                 ar & data; // Serialize first double in the key tuple
+                ar & key; // Serialize first double in the key tuple
+                ar & data; // Serialize first double in the key tuple
 
-//                 map._map[key] = data;
+                map[key] = data;
 
-//             }
-//         }
+            }
 
-//         // The serialization is split into save and load here.
-//         template<class Archive>
-//         inline void serialize(
-//             Archive & ar, 
-//             IterationMydata& map, 
-//             const unsigned int version)
-//         {
-//             split_free(ar, map, version);
-//         }
+        }
 
-//     }
-// }
+        // The serialization is split into save and load here.
+        template<class Archive, class DataType>
+        inline void serialize(
+            Archive & ar, 
+            std::unordered_map<int, DataType>& map, 
+            const unsigned int version)
+        {
+            split_free(ar, map, version);
+        }
+
+    }
+}
+
+
+// Define the serialization of iteration data type.
+namespace boost {
+   namespace serialization {  
+
+        template<class Archive, class DataType>
+        void save(Archive& ar, 
+                  const IterationData<DataType>& iter_data, 
+                  const unsigned int version) 
+        {
+            int niters=0;
+            int dim=0;
+            
+            niters = iter_data.niters();
+            dim = iter_data.dim();
+            std::unordered_map<int, DataType> map{iter_data.map()};
+
+            ar & niters; // Serialize first int key
+            ar & dim; // Serialize MyData value
+            ar & map;
+
+        }
+
+        template<class Archive, class DataType>
+        void load(Archive& ar, IterationData<DataType>& iter_data, const unsigned int version) {
+            int niters=0;
+            int dim=0;
+
+            std::unordered_map<int, DataType> map;
+
+            ar & niters; // Serialize first int key
+            ar & dim; // Serialize MyData value
+            ar & map;
+
+            iter_data.set_niters(niters);
+            iter_data.set_dim(dim);
+            iter_data.set_map(map);
+
+        }
+
+        // The serialization is split into save and load here.
+        template<class Archive, class DataType>
+        inline void serialize(
+            Archive & ar, 
+            IterationData<DataType>& map, 
+            const unsigned int version)
+        {
+            split_free(ar, map, version);
+        }
+
+    }
+}
 
--- tests/test_save_iterdata.cpp
+++ tests/test_save_iterdata.cpp
@@ -6,45 +6,97 @@
 std::string source_root{XSTRING(SOURCE_ROOT)};
 
 TEST(Serialization, initialize){
-using namespace gvi;
+    using namespace gvi;
 
-MyData d(2, 2);
-std::cout << "======== print data ========" << std::endl;
-d.print_data();
+    MyData d(2, 2);
+    std::cout << "======== print data ========" << std::endl;
+    d.print_data();
 
-Matrix3D mat3d(2,2,2);
-mat3d(0,0) = 1.0;
-mat3d(1,1) = 2.0;
-mat3d(2,1) = 3.0;
+    Matrix3D mat3d(2,2,2);
+    mat3d(0,0) = 1.0;
+    mat3d(1,1) = 2.0;
+    mat3d(2,1) = 3.0;
 
-d.update_data(mat3d);
-std::cout << "======== print data ========" << std::endl;
-d.print_data();
+    d.update_data(mat3d);
+    std::cout << "======== print saved data ========" << std::endl;
+    d.print_data();
 
-d.save_data(source_root+"/tests/test_serialize_iterdata.bin");
+    std::string fname = source_root+"/tests/test_serialize_mydata.bin";
+    // Save the tuple to a binary file
+    std::ofstream ofs(fname, std::ios::binary);
+    boost::archive::binary_oarchive oa(ofs);
+    oa << d;
+    ofs.close();
 
-MyData loaded_d;
-loaded_d.load_data(source_root+"/tests/test_serialize_iterdata.bin");
-std::cout << "======== print data ========" << std::endl;
-loaded_d.print_data();
+    // d.save_data(source_root+"/tests/test_serialize_mydata.bin");
 
-// // Create iteration data
-// IterationMydata iter_data;
+    MyData loaded_d(2,2);
+    try {
+        std::ifstream ifs(fname, std::ios::binary);
+        if (!ifs.is_open()) {
+            throw std::runtime_error("Failed to open file of iteration data. \n File: " + fname);
+        }
 
-// // Add iteration data
-// int niter = 5;
-// for (int i=0; i<niter; i++){
-//     MyData d(2, 2);
+        boost::archive::binary_iarchive ia(ifs);
+        ia >> loaded_d;
 
-//     iter_data.add_data(i, d);
-// }
+        ifs.close();
 
-// // Save data
-// iter_data.save_data(source_root+"/tests/test_serialize_iterdata.bin");
+    } catch (const boost::archive::archive_exception& e) {
+        std::cerr << "Boost archive exception: " << e.what() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << "Standard exception: " << e.what() << std::endl;
+    }
 
-// // Load data
-// IterationMydata data_loaded;
-// iter_data.load_data(source_root+"/tests/test_serialize_iterdata.bin");
-// data_loaded.print_data();
+    // loaded_d.load_data(source_root+"/tests/test_serialize_mydata.bin");
+    std::cout << "======== print loaded data ========" << std::endl;
+    loaded_d.print_data();
+
+    // Create iteration data
+    IterationMydata iter_data;
+
+    // Add iteration data
+    int niter = 5;
+    for (int i=0; i<niter; i++){
+        MyData d(2, 2);
+
+        Matrix3D mat3d(2,2,2);
+        mat3d(0,1) = i;
+        mat3d(1,1) = i+1;
+        mat3d(0,0) = i+2;
+
+        d.update_data(mat3d);
+
+        iter_data.add_data(i, d);
+    }
+    std::cout << "======== print saved iteration data ========" << std::endl;
+    iter_data.print_data();
+
+    std::string file_name = source_root+"/tests/test_serialize_iterdata.bin";
+    // Save data
+    iter_data.save_data(file_name);
+
+    // Load data
+    IterationMydata data_loaded;
+
+    // try {
+    //     std::ifstream ifs(file_name, std::ios::binary);
+    //     if (!ifs.is_open()) {
+    //         throw std::runtime_error("Failed to open file of iteration data. \n File: " + file_name);
+    //     }
+
+    //     boost::archive::binary_iarchive ia(ifs);
+    //     ia >> data_loaded;
+
+    //     ifs.close();
+
+    // } catch (const boost::archive::archive_exception& e) {
+    //     std::cerr << "Boost archive exception: " << e.what() << std::endl;
+    // } catch (const std::exception& e) {
+    //     std::cerr << "Standard exception: " << e.what() << std::endl;
+    // }
+
+    data_loaded.load_data(source_root+"/tests/test_serialize_iterdata.bin");
+    data_loaded.print_data();
 
 }
